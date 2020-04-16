b4ba2ac48919aa9c3ab344378419c16ff0d198e5
Amit Maharana <amitmaharana@asu.edu>
2020-04-15 18:31:45 -0700

    Created variant request & distributer

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { distributeVariant } from './src/utils/distributer'
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -6,7 +8,21 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
+  let response = await fetch(variantURL);
+  const variantsJson = await response.json();
+  const variants = variantsJson['variants'];
+
+  const url = distributeVariant(variants)
+  console.log(url)
+
+  response = await fetch(url)
+  const resp = await response.text()
+
+  return new Response(resp, {
+    headers: {
+      'Content-type': 'text/html'
+    },
   })
 }
+
+const variantURL = 'https://cfw-takehome.developers.workers.dev/api/variants'
